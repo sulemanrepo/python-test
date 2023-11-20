a664580eeb192df8a5011c98a485d82e510d218e
Rehan Anwar <73921545+Rehan-Rana@users.noreply.github.com>
2023-11-20 19:30:42 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World! I\'m host %s' % socket.gethostname()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
