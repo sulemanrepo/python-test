3aa53cf7dae1aee2fa60b5be9aaeafebade7f143
sulemanrepo <157711187+sulemanrepo@users.noreply.github.com>
2024-02-29 16:56:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
     return 'Hello World! This is my CodePipeline Project and Successfully Running!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
